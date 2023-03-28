59eea7710f45d7f2b33176a5fda59de393d8d010
Jens Dietrich <jens.dietrich@gmail.com>
2023-03-28 22:35:11 +1000

    added another confirmation test

--- org.yaml_snakeyaml_1.25/io.prometheus.jmx_jmx_prometheus_httpserver_java6_0.17.0/src/test/java/ConfirmVulnerabilitiesTests.java
+++ org.yaml_snakeyaml_1.25/io.prometheus.jmx_jmx_prometheus_httpserver_java6_0.17.0/src/test/java/ConfirmVulnerabilitiesTests.java
@@ -7,8 +7,16 @@ public class ConfirmVulnerabilitiesTests {
     @Test
     public void confirmCVE202238751 () {
         assertThrows(
-            StackOverflowError.class,
-            () -> Driver.parse("../payloads/CVE-2022-38751.yml")
+                StackOverflowError.class,
+                () -> Driver.parse("../payloads/CVE-2022-38751.yml")
+        );
+    }
+
+    @Test
+    public void confirmCVE202238749 () {
+        assertThrows(
+                StackOverflowError.class,
+                () -> Driver.parse("../payloads/CVE-2022-38749.yml")
         );
     }
 }
