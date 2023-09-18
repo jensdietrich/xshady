615502ccdd31a41c85bb2abb13664170a0ad9cdf
Tim White <walton.timothy.james.white@gmail.com>
2023-09-18 14:17:54 +1200

    Choose dirs more flexibly/automatically

--- tools/run_shadedetector_on_all.pl
+++ tools/run_shadedetector_on_all.pl
@@ -5,6 +5,9 @@ use warnings;
 
 my $n = 100;
 
+my $localScratchCacheDir = '/local/scratch/whitewa/shadedetector/.cache';
+my $cacheDir = (-d $localScratchCacheDir ? $localScratchCacheDir : '.');
+
 foreach my $d (<CVE-*>) {
 	my $gav = `tools/guess_gav.pl < $d/pom.xml`;
 	chomp $gav;
@@ -16,7 +19,7 @@ foreach my $d (<CVE-*>) {
 	my ($g, $a, $v) = split /:/, $gav;
 
 	# For now just assume exit status 1 means failures, not errors (it's actually the case for now).
-	my $cmd = "/usr/bin/time java -jar target/shadedetector.jar -g $g -a $a -v $v -vul ../xshady/$d -sig $sig -l log$n-$d.log -vos /home/whitewa/code/shadedetector/vuln_staging -vov /home/whitewa/code/shadedetector/vuln_final --stats stats$n-$d.log -o1 csv.details?dir=results/details$n-$d -o2 csv.summary?file=results/summary$n-$d.csv -cache /local/scratch/whitewa/shadedetector/.cache";
+	my $cmd = "/usr/bin/time java -jar target/shadedetector.jar -g $g -a $a -v $v -vul ../xshady/$d -sig $sig -l log$n-$d.log -vos $ENV{HOME}/code/shadedetector/vuln_staging -vov $ENV{HOME}/code/shadedetector/vuln_final --stats stats$n-$d.log -o1 csv.details?dir=results/details$n-$d -o2 csv.summary?file=results/summary$n-$d.csv -cache $cacheDir";
 
 	print "$cmd\n";
 
