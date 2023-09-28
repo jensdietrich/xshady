0aaf0551c4d22f1194f397b204bdcdeaa95a0e84
Tim White <walton.timothy.james.white@gmail.com>
2023-09-28 18:50:53 +1300

    Let most of the info be read automatically from pov-project.json

--- tools/run_shadedetector_on_all.pl
+++ tools/run_shadedetector_on_all.pl
@@ -11,17 +11,8 @@ my $jarPath = "../target/shadedetector.jar";
 my $xshadyPath = "$ENV{HOME}/code/xshady";
 
 foreach my $d (<CVE-*>) {
-	my $gav = `tools/guess_gav.pl < $d/pom.xml`;
-	chomp $gav;
-	my $exitStatus = `cat $d/mvn_clean_test.exitstatus`;
-	chomp $exitStatus;
-	my $sig = ($exitStatus eq '0' ? 'success' : 'failure');
-
-	#print "$d: $gav\n";
-	my ($g, $a, $v) = split /:/, $gav;
-
-	# For now just assume exit status 1 means failures, not errors (it's actually the case for now).
-	my $cmd = "/usr/bin/time java -jar $jarPath -g $g -a $a -v $v -vul $xshadyPath/$d -sig $sig -l log$n-$d.log -vos vuln_staging -vov vuln_final --stats stats$n-$d.log -o1 csv.details?dir=details$n-$d -o2 csv.summary?file=summary$n-$d.csv -cache $cacheDir";
+	# -g, -a, -v, -sig and any JAVA_HOME=... setting are now all determined from pov-project.json
+	my $cmd = "/usr/bin/time java -jar $jarPath -vul $xshadyPath/$d -l log$n-$d.log -vov vuln_final --stats stats$n-$d.log -o1 csv.details?dir=details$n-$d -o2 csv.summary?file=summary$n-$d.csv -cache $cacheDir";
 
 	print "$cmd\n";
 
