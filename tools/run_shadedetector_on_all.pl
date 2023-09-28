c6850d4458924344c26dcc0bb3776ac2d100109d
Tim White <walton.timothy.james.white@gmail.com>
2023-09-28 19:09:08 +1300

    Default to generating a Makefile; get old shell script behaviour with `--mode shell-script`

--- tools/run_shadedetector_on_all.pl
+++ tools/run_shadedetector_on_all.pl
@@ -10,11 +10,40 @@ my $cacheDir = (-d $localScratchCacheDir ? $localScratchCacheDir : "$ENV{HOME}/c
 my $jarPath = "../target/shadedetector.jar";
 my $xshadyPath = "$ENV{HOME}/code/xshady";
 
-foreach my $d (<CVE-*>) {
-	# -g, -a, -v, -sig and any JAVA_HOME=... setting are now all determined from pov-project.json
-	my $cmd = "/usr/bin/time java -jar $jarPath -vul $xshadyPath/$d -l log$n-$d.log -vov vuln_final --stats stats$n-$d.log -o1 csv.details?dir=details$n-$d -o2 csv.summary?file=summary$n-$d.csv -cache $cacheDir";
+my $mode = "make";
 
-	print "$cmd\n";
+if (@ARGV && $ARGV[0] eq '--mode') {
+	$mode = $ARGV[1];
+}
+
+die "Unknown mode" if $mode !~ /^(?:make|shell-script)$/;
+print STDERR "Generating run script in $mode mode.\n";
+
+print "# Generated at " . localtime . " by $0\n";
+
+my @targets;
+my @rules;
+
+foreach my $d (<CVE-*>) {
+	my $statsFName = "stats$n-$d.log";
+	my $pomFName = `realpath $d/pom.xml`;
+	chomp $pomFName;
+
+	# -g, -a, -v, -sig and any JAVA_HOME=... setting are now all determined from pov-project.json
+	my $cmd = "/usr/bin/time java -jar $jarPath -vul $xshadyPath/$d -l log$n-$d.log -vov vuln_final --stats $statsFName -o1 csv.details?dir=details$n-$d -o2 csv.summary?file=summary$n-$d.csv -cache $cacheDir";
+
+	if ($mode eq 'make') {
+		push @targets, $statsFName;
+		push @rules, "$statsFName: $pomFName\n\t$cmd\n\n";
+
+	} else {
+		print "$cmd\n";
+	}
 
 	++$n;
+}
+
+if ($mode eq 'make') {
+	print join(" \\\n\t", "all:", @targets), "\n\n";
+	print @rules;
 }
